2a127a13b544e2365509d428c1683204d5fe3400
ilhamridzuan <ilhamridzuaan@gmail.com>
2025-12-17 18:28:15 +0700

    Inisialisasi proyek menggunakan laravel

--- eLabora-noframework/assets/js/scripts.js
+++ eLabora-noframework/assets/js/scripts.js
@@ -34,26 +34,26 @@ function initSidebarNavigation() {
 // INDIKATOR MENU SIDEBAR AKTIF
 function highlightActiveMenu() {
     const currentPage = window.location.pathname.split("/").pop();
-  const menuItems = document.querySelectorAll(".menu-item");
+    const menuItems = document.querySelectorAll(".menu-item");
 
-  // Hilangkan dulu semua 'active'
-  menuItems.forEach((item) => item.classList.remove("active"));
+    // Hilangkan dulu semua 'active'
+    menuItems.forEach((item) => item.classList.remove("active"));
 
-  // Loop tiap item untuk menentukan yang aktif
-  menuItems.forEach((link) => {
-    const page = link.getAttribute("data-page");
+    // Loop tiap item untuk menentukan yang aktif
+    menuItems.forEach((link) => {
+        const page = link.getAttribute("data-page");
 
-    // Jika halaman antrian, tetap aktifkan menu pendaftaran
-    if (currentPage.includes("antrian") && page === "pendaftaran.html") {
-      link.classList.add("active");
-    } else if (currentPage.includes("detailPemeriksaan") && page === "hasilPemeriksaan.html") {
-      link.classList.add("active");
-    }
-    // Jika halaman normal lain, cocokkan langsung
-    else if (page === currentPage) {
-      link.classList.add("active");
-    }
-  });
+        // Jika halaman antrian, tetap aktifkan menu pendaftaran
+        if (currentPage.includes("antrian") && page === "pendaftaran.html") {
+            link.classList.add("active");
+        } else if (currentPage.includes("detailPemeriksaan") && page === "hasilPemeriksaan.html") {
+            link.classList.add("active");
+        }
+        // Jika halaman normal lain, cocokkan langsung
+        else if (page === currentPage) {
+            link.classList.add("active");
+        }
+    });
 }
 
 // INTERAKSI CARD MENU DI BERANDA
