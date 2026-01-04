59318d7dbd31360b76381d4d37eb1a6ef5bc038c
ilhamridzuan <ilhamridzuaan@gmail.com>
2026-01-05 02:30:32 +0700

    memperbarui project api dengan api yang sudah fix(sudah di deploy)

--- elabora-api/src/server.js
+++ elabora-api/src/server.js
@@ -2,4 +2,6 @@ import "dotenv/config";
 import app from "./app.js";
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`API running on http://localhost:${port}`));
+app.listen(port, "0.0.0.0", () => {
+  console.log(`API running on port ${port}`);
+});
