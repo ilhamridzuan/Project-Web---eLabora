748eef9b5eae758fdd993b9a3b6494fd9c47a2d5
ilhamridzuan <ilhamridzuaan@gmail.com>
2026-04-28 21:36:12 +0700

    delete api repository

--- elabora-api/src/server.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import app from "./app.js";
-
-const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`API running on port ${port}`);
-});
